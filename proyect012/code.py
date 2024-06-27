e2210234a0cd964adb2a158b1d139de067db3b98
antvilla06 <33465632+antvilla06@users.noreply.github.com>
2024-06-27 09:24:05 -0600

    Update code.py

--- proyect012/code.py
+++ proyect012/code.py
@@ -1,1 +1,78 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
 
+
+# Print summary statistics
+cc_apps_description = cc_apps.describe()
+print(cc_apps_description)
+print('\n')
+# Print DataFrame information
+cc_apps_info = cc_apps.info()
+print(cc_apps_info)
+print('\n')
+# Inspect missing values in the dataset
+cc_apps.tail(17)
+
+
+
+# Drop the features 11 and 13
+cc_apps = cc_apps.drop([11, 13], axis=1)
+
+# Split into train and test sets
+cc_apps_train, cc_apps_test = train_test_split(cc_apps, test_size=0.33, random_state=42)
+
+
+# Iterate over each column of cc_apps_train
+for col in cc_apps_train.columns:
+    # Check if the column is of object type
+    if cc_apps_train[col].dtypes == 'object':
+        # Impute with the most frequent value
+        cc_apps_train = cc_apps_train.fillna(cc_apps_train[col].value_counts().index[0])
+        cc_apps_test = cc_apps_test.fillna(cc_apps_train[col].value_counts().index[0])
+
+# Count the number of NaNs in the dataset and print the counts to verify
+print(cc_apps_train.isnull().sum())
+print(cc_apps_test.isnull().sum())
+
+
+
+# Replace the '?'s with NaN in the train and test sets
+cc_apps_train = cc_apps_train.replace('?', np.NaN)
+cc_apps_test = cc_apps_test.replace('?', np.NaN)
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+# Segregate features and labels into separate variables
+X_train, y_train = cc_apps_train.iloc[:, :-1].values, cc_apps_train.iloc[:, [-1]].values
+X_test, y_test = cc_apps_test.iloc[:, :-1].values, cc_apps_test.iloc[:, [-1]].values
+
+# Instantiate MinMaxScaler and use it to rescale X_train and X_test
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX_train = scaler.fit_transform(X_train)
+rescaledX_test = scaler.transform(X_test)
+
+
+# Import LogisticRegression
+from sklearn.linear_model import LogisticRegression
+
+# Instantiate a LogisticRegression classifier with default parameter values
+logreg = LogisticRegression()
+
+# Fit logreg to the train set
+logreg.fit(rescaledX_train,y_train)
+
+
+# Instantiate GridSearchCV with the required parameters
+grid_model = GridSearchCV(estimator=logreg, param_grid=param_grid, cv=5)
+
+# Fit grid_model to the data
+grid_model_result = grid_model.fit(rescaledX_train, y_train)
+
+# Summarize results
+best_score, best_params = grid_model_result.best_score_, grid_model_result.best_params_
+print("Best: %f using %s" % (best_score, best_params))
+
+# Extract the best model and evaluate it on the test set
+best_model = grid_model_result.best_estimator_
+print("Accuracy of logistic regression classifier: ", best_model.score(rescaledX_test,y_test))
