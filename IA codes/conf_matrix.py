119f9f651bb85f59cfb490ba3070da3107fbd79b
antvilla06 <33465632+antvilla06@users.noreply.github.com>
2024-06-27 09:38:38 -0600

    Update conf_matrix.py

--- IA codes/conf_matrix.py
+++ IA codes/conf_matrix.py
@@ -4,11 +4,11 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
 # Define sample labels
-true_labels = [2, 0, 0, 2, 4, 4, 1, 0, 3, 3, 3]
-pred_labels = [2, 1, 0, 2, 4, 3, 1, 0, 1, 3, 3]
+label_original = [2, 0, 58, 2, 4, 4, 1, 89, 24, 78, 14]
+label_given = [25, 1, 17, 2, 4, 27, 1, 0, 1, 3, 3]
 
 # Create confusion matrix
-confusion_mat = confusion_matrix(true_labels, pred_labels)
+confusion_mat = confusion_matrix(label_original, label_given)
 
 # Visualize confusion matrix
 plt.imshow(confusion_mat, interpolation='nearest', cmap=plt.cm.gray)
@@ -23,4 +23,4 @@ plt.show()
 
 # Classification report
 targets = ['Class-0', 'Class-1', 'Class-2', 'Class-3', 'Class-4']
-print('\n', classification_report(true_labels, pred_labels, target_names=targets))
+print('\n', classification_report(label_original, label_given, target_names=targets))
